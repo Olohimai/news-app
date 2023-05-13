481ffb646984ce136a774640ddd834be2b28c328
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-05-14 00:04:51 +0100

    code organization

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  //state
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("react");
+  const [url, setUrl] = useState("https://hn.algolia.com/api/v1/search?query=react");
+  const [loading, setLoading] = useState(false);
+  //fetch news
+  const fetchNews = () => {
+    //set loading true
+    setLoading(true);
+    fetch(url)
+      .then(result => result.json())
+      // eslint-disable-next-line no-sequences
+      .then(data => (setNews(data.hits), setLoading(false)))
+      .catch(error => console.log(error));
+  };
+  useEffect(() => {
+    fetchNews();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
+  const handleChange = e => {
+    setSearchQuery(e.target.value)
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
+  const showLoading = () => (loading?<h2>Loading....</h2> : ""
+  );
+  const searchForm = () => {
+    return <form onSubmit={handleSubmit}>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button  >Search</button>
+    </form>
+  };
+  const showNews = () => (
+    news.map((n, i) => (<p key={i}>{n.title}</p>)) 
+  )
+return (
+  <div>
+    <h2>News</h2>
+    {showLoading()}
+   {searchForm()}
+    {showNews()}
+  </div>
+);
+};
+
+export default App;
